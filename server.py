f8b63e2e2c83e6d0e774e8a3d0f69e3d21594de1
Dan Crosta <dcrosta@late.am>
2012-03-23 23:33:28 -0400

    stop using paste, deployments are simpler this way

---
+++ server.py
@@ -0,0 +1,7 @@
+from shrt import app as application
+
+if __name__ == '__main__':
+    application.run(
+        debug=True,
+        port=5000
+    )
--- shrt/__init__.py
+++ shrt/__init__.py
@@ -3,34 +3,25 @@ __all__ = ['app']
 import flask
 app = flask.Flask('shrt')
 
+from os.path import abspath, dirname, exists, join
+here = dirname(__file__)
+parent = abspath(join(dirname(__file__), '..'))
+
+config = join(parent, 'plog.cfg')
+if exists(config):
+    app.config.from_pyfile(config)
+
+private = join(parent, 'private.plog.cfg')
+if exists(private):
+    app.config.from_pyfile(private)
+
 import shrt.views
 import shrt.db
 
-def niceify(settings):
-    for key, value in settings.items():
-        if value.lower() == 'true':
-            value = True
-        elif value.lower() == 'false':
-            value = False
-        if key.endswith('_dir') or key.endswith('_path'):
-            value = abspath(value)
-        settings[key] = value
-    return settings
-
-def app_factory(global_config, **settings):
-    settings = niceify(settings)
-    app.debug = settings.pop('debug', False)
-
-    db_type = settings.pop('db_type')
-    print "using", db_type
-
-    db_kwargs = {}
-    for key in settings.keys():
-        if key.startswith(db_type):
-            value = settings.pop(key)
-            key = key[len(db_type)+1:]
-            db_kwargs[key] = value
-    shrt.db.setup(db_type=db_type, **db_kwargs)
-
-    return app
-
+db_kwargs = {}
+db_type = app.config.get('DB_TYPE', 'mongo')
+for key, value in app.config.iteritems():
+    if key.startswith(db_type.upper() + '_'):
+        key = key[len(db_type)+1:]
+        db_kwargs[key] = value
+shrt.db.setup(db_type=db_type, **db_kwargs)
