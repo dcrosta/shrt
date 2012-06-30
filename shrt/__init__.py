57cacb26f10cbc4e849254de7cc28daf3f7be860
Dan Crosta <dcrosta@late.am>
2012-06-30 12:59:40 -0400

    correct typo

--- shrt/__init__.py
+++ shrt/__init__.py
@@ -7,11 +7,11 @@ from os.path import abspath, dirname, exists, join
 here = dirname(__file__)
 parent = abspath(join(dirname(__file__), '..'))
 
-config = join(parent, 'plog.cfg')
+config = join(parent, 'shrt.cfg')
 if exists(config):
     app.config.from_pyfile(config)
 
-private = join(parent, 'private.plog.cfg')
+private = join(parent, 'private.shrt.cfg')
 if exists(private):
     app.config.from_pyfile(private)
 
